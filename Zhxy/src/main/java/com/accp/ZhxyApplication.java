d9f7544596a0e6e21ce4e1f102612d232599b62e
晨曦 <@MSI>
2018-12-12 08:46:53 +0800

    智慧校园 properties文件里的数据库密码记得修改

---
+++ Zhxy/src/main/java/com/accp/ZhxyApplication.java
@@ -0,0 +1,12 @@
+package com.accp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhxyApplication.class, args);
+	}
+}
